baf7730b87c0c63777dc293ab1be01e862a58b3a
azotamiota <azotamiota@gmail.com>
2022-02-04 14:45:07 +0000

    gutter solved ffs

--- src/DrumMachine.js
+++ src/DrumMachine.js
@@ -4,25 +4,22 @@ const DrumMachine = () => {
         <section id="header" className="text-center p-3">
           <h1>Drum machine</h1>
         </section>
-        <div id="drumbox" className="container ms-auto me-auto">
-          <div className="row m-3">
-            <button className="col-4 key">W</button>
-            <button className="col-4 key">Q</button>
-            <button className="col-4 key">E</button>
-</div>
-<div className="row m-3">
+        <div id="drumbox" className="container p-3 ms-auto me-auto">
+          <div className="row gy-1">
+            <div className="col-4 key"><button className="w-100">Key</button></div>
+            <div className="col-4 key"><button className="w-100">Key</button></div>
+            <div className="col-4 key"><button className="w-100">Key</button></div>
 
-            <button className="col key">A</button>
-            <button className="col key">key1</button>
-            <button className="col key">key1</button>
 
-            </div>
+            <div className="col-4 key"><button className="w-100">Key</button></div>
+            <div className="col-4 key"><button className="w-100">Key</button></div>
+            <div className="col-4 key"><button className="w-100">Key</button></div>
 
-<div className="row m-3">
+     
 
-            <button className="col key">key1</button>
-            <button className="col key">key1</button>
-            <button className="col key">key1</button>
+            <div className="col-4 key"><button className="w-100">Key</button></div>
+            <div className="col-4 key"><button className="w-100">Key</button></div>
+            <div className="col-4 key"><button className="w-100">Key</button></div>
             </div>
           </div>
         
