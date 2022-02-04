c1b75bf51897a8f3b85d89d846f3b0de4977b114
azotamiota <azotamiota@gmail.com>
2022-02-04 20:53:09 +0000

    first sound added

--- src/DrumMachine.js
+++ src/DrumMachine.js
@@ -1,4 +1,18 @@
 const DrumMachine = () => {
+
+const kick = new Audio(
+  "https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3"
+);
+
+const handleClick = (e) => {
+console.log('button has been pressed')
+kick.play()
+}
+
+const handleKeyPress = (e) => {
+
+}
+
     return (
       <>
         <section id="header" className="text-center p-3">
@@ -6,33 +20,33 @@ const DrumMachine = () => {
         </section>
         <div id="drumbox" className="container">
           <div className="key-container">
-            <div className="key">
+            <div className="key" id='key1' onClick={handleClick}>
               <div className="btn">Key</div>
             </div>
-            <div className="key">
+            <div className="key" id='key2' onClick={handleClick}>
               <div className="btn">Key</div>
             </div>
-            <div className="key">
-              <div className="btn">Key</div>
-            </div>
-
-            <div className="key">
-              <div className="btn">Key</div>
-            </div>
-            <div className="key">
-              <div className="btn">Key</div>
-            </div>
-            <div className="key">
+            <div className="key" id='key3' onClick={handleClick}>
               <div className="btn">Key</div>
             </div>
 
-            <div className="key">
+            <div className="key" id='key4' onClick={handleClick}>
               <div className="btn">Key</div>
             </div>
-            <div className="key">
+            <div className="key" id='key5' onClick={handleClick}>
               <div className="btn">Key</div>
             </div>
-            <div className="key">
+            <div className="key" id='key6' onClick={handleClick}>
+              <div className="btn">Key</div>
+            </div>
+
+            <div className="key" id='key7' onClick={handleClick}>
+              <div className="btn">Key</div>
+            </div>
+            <div className="key" id='key8' onClick={handleClick}>
+              <div className="btn">Key</div>
+            </div>
+            <div className="key" id='key9' onClick={handleClick}>
               <div className="btn">Key</div>
             </div>
           </div>
