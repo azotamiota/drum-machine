9efbe620e5c15dac1f549fc3f5158ebc87f8cac7
azotamiota <azotamiota@gmail.com>
2022-02-04 22:02:18 +0000

    sounds added for key pressing

--- src/DrumMachine.js
+++ src/DrumMachine.js
@@ -1,17 +1,62 @@
 const DrumMachine = () => {
 
+document.addEventListener('keydown', function(event) {
+    //console.log(`The ${event.key} has been pressed, keycode: ${event.keyCode}`);
+    if (event.key === "q") {
+      heater1.play();
+    } else if (event.key === "w") {
+      heater2.play();
+    } else if (event.key === "e") {
+      heater3.play();
+    } else if (event.key === "a") {
+      heater4.play();
+    } else if (event.key === "s") {
+      clap.play();
+    } else if (event.key === "d") {
+      openHH.play();
+    } else if (event.key === "z") {
+      kickNHat.play();
+    } else if (event.key === "x") {
+      kick.play();
+    } else if (event.key === "c") {
+      closedHH.play();
+    } else {
+        console.log('No sound for this key')
+    }
+})
+const heater1 = new Audio(
+  "https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3"
+);
+const heater2 = new Audio(
+  "https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3"
+);
+const heater3 = new Audio(
+  "https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3"
+);
+const heater4 = new Audio(
+  "https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3"
+);
+const clap = new Audio(
+  "https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3"
+);
+const openHH = new Audio(
+  "https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3"
+);
+const kickNHat = new Audio(
+  "https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3"
+);
 const kick = new Audio(
   "https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3"
 );
+const closedHH = new Audio(
+  "https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3"
+);
 
-const handleClick = (e) => {
-console.log('button has been pressed')
-kick.play()
-}
+// const handleClick = (e) => {
+// console.log('button has been pressed')
+// kick.play()
+// }
 
-const handleKeyPress = (e) => {
-
-}
 
     return (
       <>
@@ -20,36 +65,37 @@ const handleKeyPress = (e) => {
         </section>
         <div id="drumbox" className="container">
           <div className="key-container">
-            <div className="key" id='key1' onClick={handleClick}>
+            <button className="key" id="key1">
+              <div className="btn">Key</div>
+            </button>
+            <div className="key" id="key2">
               <div className="btn">Key</div>
             </div>
-            <div className="key" id='key2' onClick={handleClick}>
-              <div className="btn">Key</div>
-            </div>
-            <div className="key" id='key3' onClick={handleClick}>
-              <div className="btn">Key</div>
-            </div>
-
-            <div className="key" id='key4' onClick={handleClick}>
-              <div className="btn">Key</div>
-            </div>
-            <div className="key" id='key5' onClick={handleClick}>
-              <div className="btn">Key</div>
-            </div>
-            <div className="key" id='key6' onClick={handleClick}>
+            <div className="key" id="key3">
               <div className="btn">Key</div>
             </div>
 
-            <div className="key" id='key7' onClick={handleClick}>
+            <div className="key" id="key4">
               <div className="btn">Key</div>
             </div>
-            <div className="key" id='key8' onClick={handleClick}>
+            <div className="key" id="key5">
               <div className="btn">Key</div>
             </div>
-            <div className="key" id='key9' onClick={handleClick}>
+            <div className="key" id="key6">
+              <div className="btn">Key</div>
+            </div>
+
+            <div className="key" id="key7">
+              <div className="btn">Key</div>
+            </div>
+            <div className="key" id="key8">
+              <div className="btn">Key</div>
+            </div>
+            <div className="key" id="key9">
               <div className="btn">Key</div>
             </div>
           </div>
+          
         </div>
       </>
     ); 
