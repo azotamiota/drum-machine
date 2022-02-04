f0779e437e91930d1477375e8074c7dd52d10bd7
azotamiota <azotamiota@gmail.com>
2022-02-04 13:47:16 +0000

    buttons created

--- src/DrumMachine.js
+++ src/DrumMachine.js
@@ -1,5 +1,33 @@
 const DrumMachine = () => {
-    return <h1>Drum machine!</h1>
+    return (
+      <>
+        <section id="header" className="text-center p-3">
+          <h1>Drum machine</h1>
+        </section>
+        <div id="drumbox" className="container ms-auto me-auto">
+          <div className="row m-3">
+            <button className="col-4 key">W</button>
+            <button className="col-4 key">Q</button>
+            <button className="col-4 key">E</button>
+</div>
+<div className="row m-3">
+
+            <button className="col key">A</button>
+            <button className="col key">key1</button>
+            <button className="col key">key1</button>
+
+            </div>
+
+<div className="row m-3">
+
+            <button className="col key">key1</button>
+            <button className="col key">key1</button>
+            <button className="col key">key1</button>
+            </div>
+          </div>
+        
+      </>
+    ); 
 }
 
 export default DrumMachine;
