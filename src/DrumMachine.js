35daca3758cf9b4ffc4192222eed8e7af93413ee
azotamiota <azotamiota@gmail.com>
2022-02-05 15:21:34 +0000

    test passed

--- src/DrumMachine.js
+++ src/DrumMachine.js
@@ -1,60 +1,89 @@
 const DrumMachine = () => {
 
-document.addEventListener('keydown', function(event) {
-    //console.log(`The ${event.key} has been pressed, keycode: ${event.keyCode}`);
-    if (event.key === "q") {
-      heater1.play();
-    } else if (event.key === "w") {
-      heater2.play();
-    } else if (event.key === "e") {
-      heater3.play();
-    } else if (event.key === "a") {
-      heater4.play();
-    } else if (event.key === "s") {
-      clap.play();
-    } else if (event.key === "d") {
-      openHH.play();
-    } else if (event.key === "z") {
-      kickNHat.play();
-    } else if (event.key === "x") {
-      kick.play();
-    } else if (event.key === "c") {
-      closedHH.play();
-    } else {
-        console.log('No sound for this key')
-    }
-})
-const heater1 = new Audio(
-  "https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3"
-);
-const heater2 = new Audio(
-  "https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3"
-);
-const heater3 = new Audio(
-  "https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3"
-);
-const heater4 = new Audio(
-  "https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3"
-);
-const clap = new Audio(
-  "https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3"
-);
-const openHH = new Audio(
-  "https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3"
-);
-const kickNHat = new Audio(
-  "https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3"
-);
-const kick = new Audio(
-  "https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3"
-);
-const closedHH = new Audio(
-  "https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3"
-);
+const soundQ = () => {
+document.getElementById("Q").play();
+    document.getElementById('display').innerText = document.getElementById("keyQ").getAttribute('name');
+}
 
-// const handleClick = (e) => {
+const soundW = () => {
+  document.getElementById("W").play();
+  document.getElementById("display").innerText = document
+    .getElementById("keyW")
+    .getAttribute("name");
+};const soundE = () => {
+  document.getElementById("E").play();
+  document.getElementById("display").innerText = document
+    .getElementById("keyE")
+    .getAttribute("name");
+};const soundA = () => {
+  document.getElementById("A").play();
+  document.getElementById("display").innerText = document
+    .getElementById("keyA")
+    .getAttribute("name");
+};const soundS = () => {
+  document.getElementById("S").play();
+  document.getElementById("display").innerText = document
+    .getElementById("keyS")
+    .getAttribute("name");
+};const soundD = () => {
+  document.getElementById("D").play();
+  document.getElementById("display").innerText = document
+    .getElementById("keyD")
+    .getAttribute("name");
+};const soundZ = () => {
+  document.getElementById("Z").play();
+  document.getElementById("display").innerText = document
+    .getElementById("keyZ")
+    .getAttribute("name");
+};const soundX = () => {
+  document.getElementById("X").play();
+  document.getElementById("display").innerText = document
+    .getElementById("keyX")
+    .getAttribute("name");
+};const soundC = () => {
+  document.getElementById("C").play();
+  document.getElementById("display").innerText = document
+    .getElementById("keyC")
+    .getAttribute("name");
+};
+document.addEventListener("keydown", function (event) {
+  //console.log(`The ${event.key} has been pressed, keycode: ${event.keyCode}`);
+  console.log('child element for keyQ: ',document.getElementById('keyQ').children[0])
+  if (event.keyCode === 81) {
+    soundQ()
+    
+  } else if (event.keyCode === 87) {
+    soundW()
+
+  } else if (event.keyCode === 69) {
+    soundE()
+
+  } else if (event.keyCode === 65) {
+    soundA()
+
+  } else if (event.keyCode === 83) {
+    soundS() 
+
+  } else if (event.keyCode === 68) {
+    soundD()
+
+  } else if (event.keyCode === 90) {
+    soundZ()
+
+  } else if (event.keyCode === 88) {
+    soundX()
+  
+  } else if (event.keyCode === 67) {
+    soundC()
+
+  } else {
+    console.log("No sound for this key");
+  }
+});
+
+// const handleClick = (key) => {
 // console.log('button has been pressed')
-// kick.play()
+// document.getElementById(key).play()
 // }
 
 
@@ -63,39 +92,145 @@ const closedHH = new Audio(
         <section id="header" className="text-center p-3">
           <h1>Drum machine</h1>
         </section>
-        <div id="drumbox" className="container">
+        <div id="drum-machine" className="container">
           <div className="key-container">
-            <button className="key" id="key1">
-              <div className="btn">Key</div>
-            </button>
-            <div className="key" id="key2">
-              <div className="btn">Key</div>
+            <div
+              className="drum-pad"
+              id="keyQ"
+              name="Heater-1"
+              onClick={soundQ}
+            >
+              <audio
+                src="https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3"
+                className="clip"
+                id="Q"
+              ></audio>
+              Q
             </div>
-            <div className="key" id="key3">
-              <div className="btn">Key</div>
+            <div
+              className="drum-pad"
+              id="keyW"
+              name="Heater-2"
+              onClick={
+                () => soundW()
+              }
+            >
+              <audio
+                src="https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3"
+                className="clip"
+                id="W"
+              ></audio>
+              W
+            </div>
+            <div
+              className="drum-pad"
+              id="keyE"
+              name="Heater-3"
+              onClick={
+                () => soundE()
+              }
+            >
+              <audio
+                src="https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3"
+                className="clip"
+                id="E"
+              ></audio>
+              E
             </div>
 
-            <div className="key" id="key4">
-              <div className="btn">Key</div>
+            <div
+              className="drum-pad"
+              id="keyA"
+              name="Heater-4"
+              onClick={
+                () => soundA()
+              }
+            >
+              <audio
+                src="https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3"
+                className="clip"
+                id="A"
+              ></audio>
+              A
             </div>
-            <div className="key" id="key5">
-              <div className="btn">Key</div>
+            <div
+              className="drum-pad"
+              id="keyS"
+              name="Clap"
+              onClick={
+                () => soundS()
+              }
+            >
+              <audio
+                src="https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3"
+                className="clip"
+                id="S"
+              ></audio>
+              S
             </div>
-            <div className="key" id="key6">
-              <div className="btn">Key</div>
+            <div
+              className="drum-pad"
+              id="keyD"
+              name="Open-HH"
+              onClick={
+                () => soundD()
+              }
+            >
+              <audio
+                src="https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3"
+                className="clip"
+                id="D"
+              ></audio>
+              D
             </div>
 
-            <div className="key" id="key7">
-              <div className="btn">Key</div>
+            <div
+              className="drum-pad"
+              id="keyZ"
+              name="Kick'n Hat"
+              onClick={
+                () => soundZ()
+              }
+            >
+              <audio
+                src="https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3"
+                className="clip"
+                id="Z"
+              ></audio>
+              Z
             </div>
-            <div className="key" id="key8">
-              <div className="btn">Key</div>
+            <div
+              className="drum-pad"
+              id="keyX"
+              name="Kick"
+              onClick={
+                () => soundX()
+              }
+            >
+              <audio
+                src="https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3"
+                className="clip"
+                id="X"
+              ></audio>
+              X
             </div>
-            <div className="key" id="key9">
-              <div className="btn">Key</div>
+            <div
+              className="drum-pad"
+              id="keyC"
+              name="Closed-HH"
+              onClick={
+                () => soundC()
+              }
+            >
+              <audio
+                src="https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3"
+                className="clip"
+                id="C"
+              ></audio>
+              C
             </div>
           </div>
-          
+          <section id="display">Sound name</section>
         </div>
       </>
     ); 
